842ab69b22a1b07abada75be17742c9eee80d41d
qiuqili <835671347@qq.com>
2017-03-20 08:20:19 -0400

    animation

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var tl = new TimelineLite();
-tl.from(".title",2,{opacity:0,x:-100,ease:Bounce.easeOut},0)
-  .from("ul",2,{opacity:0,x:100,ease:Bounce.easeOut},0)
-  .staggerFrom("span,.component h1,p",1,{scale:0,opacity:0,ease:Back.easeOut},0.2,0)
-  .staggerFrom(".details,.component img",1,{scsle:0,opacity:0,x:100,ease:Back.easeInOut},0.2,"0.8");
+tl.from(".title",2,{opacity:0,x:-100,ease:Back.easeOut},0)
+  .from("ul",2,{opacity:0,x:100,ease:Back.easeOut},0)
+  .staggerFrom("span,.component h1,p",0.5,{scale:0,opacity:0,ease:Circ.easeOut},0.1,0)
+  .staggerFrom(".details,.component img",1,{scale:0,opacity:0,x:100,ease:Circ.easeOut},0.1,"0.3");
