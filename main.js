13b33ab9ebf4fa6bd9339c792e32a0e90ac375b5
Rachel Li <835671347@qq.com>
2017-04-10 14:35:25 -0400

    animation

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var tl = new TimelineLite();
 tl.from(".title",2,{opacity:0,x:-100,ease:Back.easeOut},0)
   .from("ul",2,{opacity:0,x:100,ease:Back.easeOut},0)
-  .staggerFrom("span,.component h1,p",0.5,{scale:0,opacity:0,ease:Circ.easeOut},0.1,0)
-  .staggerFrom(".details,.component img",1,{scale:0,opacity:0,x:100,ease:Circ.easeOut},0.1,"0.3");
+  .staggerFrom("span,.component h1,p",1,{opacity:0,ease:Circ.easeOut},0.1,0)
+  .staggerFrom(".details,.component img",1,{opacity:0,x:100,ease:Circ.easeOut},0.1,"0.3");
